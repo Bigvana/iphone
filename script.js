c60e2256fdc08202ef9e72a55e76c64b414e9174
Bigvana <82066545+Bigvana@users.noreply.github.com>
2024-11-05 16:33:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var randomlinks = [];
+randomlinks[0]="https://www.bigvana.com/2023/12/kumpulan-caption-bahasa-inggris-dan.html";
+randomlinks[1]="https://www.bigvana.com/2023/12/kumpulan-kata-kata-atau-caption-untuk.html";
+randomlinks[2]="https://www.bigvana.com/2022/12/cara-mendowload-video-pada-platform.html";
+randomlinks[3]="https://www.bigvana.com/2022/12/manfaat-membayar-pajak-bagi-masyarakat.html";
+randomlinks[4]="https://www.bigvana.com/2022/11/tips-memilih-oli-untuk-motor-matic.html";
+randomlinks[5]="https://www.bigvana.com/2022/11/tips-menjawab-soal-saat-test-seleksi-p3k.html";
+randomlinks[6]="https://www.bigvana.com/2022/11/cara-belajar-perkalian-yang-mudah-dan.html";
+randomlinks[7]="https://www.bigvana.com/2022/07/viral-video-wajah-resty-kejora-seperti.html";
+randomlinks[8]="https://www.bigvana.com/2022/08/apakah-kehidupan-seks-anda-mulai.html";
+randomlinks[9]="https://www.bigvana.com/2022/08/cara-berhubungan-badan-yang-sehat.html";
+randomlinks[10]="https://www.bigvana.com/2022/08/cara-investasi-untuk-pemula.html";
+randomlinks[11]="https://www.bigvana.com/2022/07/how-to-add-text-box-to-blogger-article.html";
+
+function randomlink(){
+  window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
+}
